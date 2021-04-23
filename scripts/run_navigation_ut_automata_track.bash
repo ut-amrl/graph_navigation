56fce2ce12702748133b58112197620c88f41d9e
Tongrui Li <tongrui1998@gmail.com>
2021-04-22 19:32:35 -0500

    new track

--- scripts/run_navigation_ut_automata_track.bash
+++ scripts/run_navigation_ut_automata_track.bash
@@ -10,7 +10,7 @@ MAX_CURVATURE=.9 #Maximum curvature of turning) type: double default: 2
 MAX_DECEL=5 #(Maximum deceleration) type: double default: 1
 MAX_PLAN_DEVIATION=0.5 #(Maximum premissible deviation from the plan) type: double default: 0.5
 MAX_SPEED=2.0 #(Maximum speed) type: double default: 0.5
-NO_JOYSTICK="false" # (Disregards autonomy enable mode from joystick) type: bool default: false
+NO_JOYSTICK="true" # (Disregards autonomy enable mode from joystick) type: bool default: false
 NUM_OPTIONS=41 # (Number of options to consider) type: int32 default: 41
 OBSTACLE_MARGIN=0.149 # (Margin to leave for obstacle avoidance) type: double default: 0.14999999999999999
 SUB_OPT=1.5 # (Max path increase for clearance) type: double default: 1.5
@@ -23,8 +23,8 @@ ENABLE_TOPIC="/autonomy_enabler" # enable_topic (ROS topic that indicates whethe
 
 
 ../bin/navigation \
- --map="UT_AUTOmata_Track" \
- --maps_dir="/home/amrl_user/amrl_maps" \
+ --map="UT_AUTOmata_Track_New" \
+ --maps_dir="/home/mango/Academics/AMRL/amrl_maps" \
  --odom_topic="/odom" \
  --carrot_dist=$CARROT_DIST \
  --cw=$CLEARANCE_WEIGHT \
