31afdfa2d37bec1958b30d178d3034eec7584276
Kavan Sikand <kvsikand@gmail.com>
2021-08-19 15:00:04 -0400

    fix command

--- setup_nav_env.sh
+++ setup_nav_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-tmux new-session -d -s kinect '~/k4a_ros/bin/depth_to_lidar -rgb'
+tmux new-session -d -s kinect 'cd ~/k4a_ros && ./bin/depth_to_lidar -rgb'
 tmux new-session -d -s repub 'rosrun image_transport republish raw in:=/vis_image compressed out:=/vis_image'
 rosnode kill navigation
