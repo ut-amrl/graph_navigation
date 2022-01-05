88a228139e4aff519934f92b4a81d9b267d0fb3b
Kavan Sikand <kvsikand@gmail.com>
2022-01-05 11:56:47 -0600

    fix default config

--- config/navigation.lua
+++ config/navigation.lua
@@ -36,9 +36,9 @@ NavigationParameters = {
   target_angle_tolerance = 0.05;
   local_fov = deg2rad(90);
   use_kinect = true;
-  camera_calibration_path = "config/camera_calibration.yaml";
+  camera_calibration_path = "config/camera_calibration_kinect.yaml";
   model_path = "../preference_learning_models/jit_cost_model_outdoor_6dim.pt";
-  evaluator_type = "cost_map";
+  evaluator_type = "linear";
 };
 
 AckermannSampler = {
