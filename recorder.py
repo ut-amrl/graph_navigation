0708f656040c243e24e01d23b6d0bfa66c2724a7
Rahul Menon <rmeno12@utexas.edu>
2023-03-02 14:59:51 -0600

    fix recorder

--- recorder.py
+++ recorder.py
@@ -7,13 +7,11 @@ import roslib
 roslib.load_manifest("graph_navigation")
 
 import rospy
-from std_msgs.msg import Joy
+from sensor_msgs.msg import Joy
 
 
 class Recorder:
     recording = False
-    count = 0
-    date = datetime.now().strftime("%Y-%m-%d")
     topics = "/bev/single/compressed /joint_states /odometry/filtered /tf /jackal_velocity_controller/cmd_vel /camera/rgb/image_raw/compressed".split()
     proc = None
 
@@ -24,19 +22,17 @@ def joy_callback(msg: Joy):
 
     if startbutton == 1 and not Recorder.recording:
         Recorder.recording = True
-        print(f"Starting recording {Recorder.date}_{Recorder.count}")
+        print("Starting recording")
         Recorder.proc = subprocess.Popen(
             [
                 "/opt/ros/noetic/bin/rosbag",
                 "record",
-                "-O",
-                f"{Recorder.date}_{Recorder.count}",
                 *Recorder.topics,
             ]
         )
     elif endbutton == 1 and Recorder.recording:
         Recorder.proc.send_signal(signal.SIGINT)
-        print(f"Finished recording {Recorder.date}_{Recorder.count}")
+        print("Finished recording")
         Recorder.count += 1
         Recorder.recording = False
 
@@ -46,3 +42,6 @@ def main():
 
     rospy.Subscriber("/bluetooth_teleop/joy", Joy, joy_callback)
     rospy.spin()
+
+if __name__ == "__main__":
+    main()
