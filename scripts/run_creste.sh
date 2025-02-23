484900b94b1bda97ef41c1e696126e10b0171862
artzha <arthurz@cs.utexas.edu>
2025-02-22 20:23:15 -0600

    Added creste run scripts for more robustness

--- scripts/run_creste.sh
+++ scripts/run_creste.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./bin/navigation --robot_config config/navigation_creste_mueller_longhorizon.lua -v 2
+./bin/navigation --robot_config config/navigation_creste.lua -v 2
---
+++ scripts/run_creste_longhorizon.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./bin/navigation --robot_config config/navigation_creste_mueller_longhorizon.lua -v 2
