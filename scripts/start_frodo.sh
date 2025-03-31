a6663ee74f2d3a440eb02c2ddfbd6c5276b68290
artzha <arthurz@cs.utexas.edu>
2025-03-31 16:59:24 -0500

    Added creste dependent frodo startup

--- scripts/start_frodo.sh
+++ scripts/start_frodo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 colcon build --packages-select amrl_msgs amrl_maps graph_navigation
 source install/setup.bash
-ros2 run graph_navigation navigation
+cd /frodo_autonomy/src/graph_navigation && ros2 run graph_navigation navigation --robot_config config/navigation_frodo_creste.lua
